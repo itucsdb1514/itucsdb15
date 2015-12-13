1712431c5ab7e816a86638096719b48f1369361c
huseyinyavuz <huseyinyavuz@outlook.com>
2015-12-13 03:16:52 +0200

    Added team reference(foreign key) to leagues table

--- pages/leagueslist.py
+++ pages/leagueslist.py
@@ -1,5 +1,5 @@
 from flask import url_for,redirect,render_template,request
-from tables import leagues
+from tables import leagues,teams
 import datetime
 
 
@@ -7,8 +7,10 @@ def leaguesList(dsn):
     leagueTable = leagues.Leagues(dsn)
     if request.method == 'GET':
         now = datetime.datetime.now()
-        data=leagueTable.select_leagues()
-        return render_template('leagues.html', current_time=now.ctime(),rows=data, update=False)
+        teamsTable=teams.Teams(dsn)
+        data2=teamsTable.select_teams()
+        data=leagueTable.select_Joint_League()
+        return render_template('leagues.html', current_time=now.ctime(),rows=data, update=False,TeamsSelect=data2)
     elif 'Delete' in request.form:
         keys = request.form.getlist('movies_to_delete')
         for key in keys:
@@ -16,10 +18,11 @@ def leaguesList(dsn):
         leagueTable.close_con()
         return redirect(url_for('leaguesList'))
     elif 'Add' in request.form:
+        team=request.form['SelectTeamName']
         name=request.form['Name']
         country=request.form['Country']
         year=request.form['Year']
-        leagueTable.add_league(name,country,year)
+        leagueTable.add_league(team,name,country,year)
         leagueTable.close_con()
         return redirect(url_for('leaguesList'))
     elif 'Update2' in request.form:
@@ -28,25 +31,27 @@ def leaguesList(dsn):
            name=request.form['Name'+key]
            country=request.form['Country'+key]
            year=request.form['Year'+key]
-           leagueTable.update_league(key, name, country, year)
+           leagueTable.update_league(key,name,country,year)
         leagueTable.close_con()
         return redirect(url_for('leaguesListUpdate'))
     elif 'Find' in request.form:
         now = datetime.datetime.now()
+        team=request.form['TeamF']
         name=request.form['NameF']
         country=request.form['CountryF']
         year=request.form['YearF']
-        data=leagueTable.find_leagues(name, country, year)
-        temp=render_template('leagues.html', current_time=now.ctime(),rows=data, update=False)
+        data=leagueTable.find_Joint_League(team,name,country,year)
+        teamsTable=teams.Teams(dsn)
+        data2 =teamsTable.select_teams()
+        temp=render_template('leagues.html', current_time=now.ctime(),rows=data, update=False,TeamsSelect=data2)
         leagueTable.close_con()
         return temp
-
 
 def updateLeaguesList(dsn):
     leagueTable = leagues.Leagues(dsn)
     if request.method == 'GET':
         now = datetime.datetime.now()
-        data=leagueTable.select_leagues()
+        data=leagueTable.select_Joint_League()
         temp=render_template('leagues.html', current_time=now.ctime(),rows=data, update=True)
         leagueTable.close_con()
         return temp
--- tables/leagues.py
+++ tables/leagues.py
@@ -1,5 +1,5 @@
 import psycopg2 as dbapi2
-
+from asyncio.locks import Condition
 
 class Leagues:
     def __init__(self, dsn):
@@ -14,18 +14,20 @@ class Leagues:
         except dbapi2.DatabaseError:
             self.connection.rollback()
         try:
+
             stat1 = """ CREATE TABLE Leagues (
                 ID SERIAL PRIMARY KEY,
                 NAME VARCHAR(40),
                 COUNTRY VARCHAR(40),
-                YEAR INTEGER
+                YEAR INTEGER,
+                FK_TeamsID INTEGER REFERENCES Teams ON DELETE CASCADE ON UPDATE CASCADE
                 ) """
             self.cursor.execute(stat1)
-            stat1 = """ INSERT INTO Leagues (NAME, COUNTRY, YEAR) VALUES('Major League Baseball', 'USA', 1903)"""
+            stat1 = """ INSERT INTO Leagues (NAME, COUNTRY, YEAR, FK_TeamsID) VALUES('Major League Baseball', 'USA', 1903,1)"""
             self.cursor.execute(stat1)
-            stat1 = """ INSERT INTO Leagues (NAME, COUNTRY, YEAR) VALUES('Minor League Baseball', 'USA', 1868)"""
+            stat1 = """ INSERT INTO Leagues (NAME, COUNTRY, YEAR, FK_TeamsID) VALUES('Minor League Baseball', 'USA', 1868,2)"""
             self.cursor.execute(stat1)
-            stat1 = """ INSERT INTO Leagues (NAME, COUNTRY, YEAR) VALUES('Dominican Professional Baseball League', 'DOM', 1951)"""
+            stat1 = """ INSERT INTO Leagues (NAME, COUNTRY, YEAR, FK_TeamsID) VALUES('Dominican Professional Baseball League', 'DOM', 1951,3)"""
             self.cursor.execute(stat1)
             self.connection.commit()
         except dbapi2.DatabaseError:
@@ -36,13 +38,7 @@ class Leagues:
         self.cursor.execute(statement)
         return self.cursor
 
-    def update_league(self, Id, name, country, year):
-        statement = """UPDATE Leagues SET NAME = '{}', COUNTRY = '{}', YEAR = {} WHERE ID = {}""".format( name, country, year, Id)
-        self.cursor.execute(statement)
-        self.connection.commit()
-
-
-    def find_leagues(self, name, country, year):
+    def find_Leagues(self,team,name,country,year):
         condition=''
         if(name.strip()):
             condition+=""" NAME LIKE '%{}%' """.format(name)
@@ -55,23 +51,55 @@ class Leagues:
                 condition+='AND'
             condition+=""" YEAR = {} """.format(year)
         if(condition.strip()):
-            condition=' WHERE '+ condition
+            condition=' WHERE '+condition
 
         statement = """ SELECT * FROM Leagues """+condition
         self.cursor.execute(statement)
         return self.cursor
-
 
     def delete_league(self,Id):
         stement =""" DELETE FROM Leagues WHERE ID={}""".format(Id)
         self.cursor.execute(stement)
         self.connection.commit()
 
-    def add_league(self, name, country, year):
+    def add_league(self, team, name, country, year):
         if(name.strip() and country.strip() ):
-            statement = """ INSERT INTO Leagues (NAME, COUNTRY, YEAR) VALUES('{}','{}',{})""".format(name, country, year)
+            statement = """ INSERT INTO Leagues (FK_TeamsID, NAME, COUNTRY, YEAR) VALUES({},'{}','{}',{})""".format(team, name, country, year)
             self.cursor.execute(statement)
             self.connection.commit()
+
+    def update_league(self, Id, name, country, year):
+        statement = """UPDATE Leagues SET NAME = '{}', COUNTRY = '{}', YEAR = {} WHERE ID = {}""".format( name, country, year, Id)
+        self.cursor.execute(statement)
+        self.connection.commit()
+
+
+    def select_Joint_League(self):
+        statement = """ SELECT Leagues.ID,Teams.Name,Leagues.NAME,Leagues.COUNTRY,Leagues.YEAR FROM Leagues INNER JOIN Teams ON Teams.ID=Leagues.FK_TeamsID  """
+        self.cursor.execute(statement)
+        return self.cursor
+
+    def find_Joint_League(self,team,name,country,year):
+        statement = """ SELECT Leagues.ID,Teams.Name,Leagues.NAME,Leagues.COUNTRY,Leagues.YEAR FROM Leagues INNER JOIN Teams ON Teams.ID=Leagues.FK_TeamsID  """
+        condition=''
+        if(team.strip()):
+            condition+=""" Teams.Name LIKE '%{}%'""".format(team)
+        if(name.strip()):
+            if(condition.strip()):
+                condition+='AND'
+            condition+=""" Leagues.NAME LIKE '%{}%' """.format(name)
+        if(country.strip()):
+            if(condition.strip()):
+                condition+='AND'
+            condition+=""" Leagues.COUNTRY LIKE '%{}%' """.format(country)
+        if(year.strip()):
+            if(condition.strip()):
+                condition+='AND'
+            condition+=""" Leagues.YEAR={} """.format(year)
+        if(condition.strip()):
+            condition=' WHERE '+condition
+        self.cursor.execute(statement+condition)
+        return self.cursor
 
     def close_con(self):
         self.connection.close()
